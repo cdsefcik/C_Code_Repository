41364794d9b9be000e2877c58e8f0f3a57031def
Christopher Sefcik <cdsefcik@gmail.com>
2025-02-18 06:59:22 -0500

    Update MemoryAllocation.c

--- MemoryAllocation.c
+++ MemoryAllocation.c
@@ -1,41 +1,76 @@
-#include<iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
 
-using namespace std;
 
 /*
-* Calculate the number of zeros in a number.
-* Provides an example of programming with recursion. 
+* Malloc: Allocates size bytes of uninitialized storage.
+* Calloc: Allocates memory for an array of num objects of size and initializes all bytes in the storage to zero.
+* Realloc: Re allocates a portion of memory. Must have been previously assigend using Malloc or Calloc.
+* free: Frees the memory.
+* This uses the memory allocation functions to get lines from the user.
 */
 
-int countZeros(int n) {
-	//base case
-	if (n == 0) {
-		return 0;
-	}
+char *get_line_alloc(int *plen){
+    if(!plen){ //Same as plan -- NULL
 
-	//recursive case
-	int smallAns = countZeros(n / 10);
+    //The use does not care about the length.
+    plen = malloc(sizeof(*plen));
+    if(!plen){
+        //Cannot allocate memory... failure
+        return NULL;
+    }
+    }
+    *plen = 0;
+    char *line = NULL;
+    int buffer_size = 0, input;
+    const int alloc_block_size = 5; //This will be the size of the buffer needed as the requirements grow.
 
-	//calculation
-	int last_digit = n % 10;
-	if (last_digit == 0) {
-		return 1 + smallAns;
-	}
-	else {
-		return smallAns;
-	}
-    
+    while(true){
+       input = getchar();
+
+       if(buffer_size <= *plen){
+        buffer_size += alloc_block_size;
+        char *realloc_line = realloc(line, buffer_size);
+
+        if(!realloc_line){
+            //Cannont do reallocation... failure.
+            free(line);
+            return NULL;
+        }
+        line = realloc_line;
+
+       }
+
+       if(input != '\n' && input != EOF){
+        line[(*plen)++] = (char)input;
+       } else{
+        break;
+       }
+    }
+    line[*plen] = '\0';
+    return line;
 }
 
+int main(){
 
-int main() {
+char *line = NULL; //pointer to where the line is stored
+int len = -1;
 
-	    int a;
-		cout << "Enter the number: ";
-		cin >> a;
-		
-		cout << countZeros(a);
+do{
+        printf("Enter a line (END to quit):\n");
+        free(line);
+        line = get_line_alloc(NULL);
 
-		return 0;
+        if(line){
+            printf("%s ()\n",line);
 
-}
+        }else{
+            printf("Cannot allocate memory\n");
+        }
+
+}while(line != NULL && strcmp(line, "END"));
+
+    return EXIT_SUCCESS;
+};
